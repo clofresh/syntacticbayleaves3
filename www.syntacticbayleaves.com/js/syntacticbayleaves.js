a21cf3c889e4a47005d11b637f0cd3b448f64c7b
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2018-02-15 11:11:52 -0800

    remove jquery, and bootstrap js, inline the go-home js

--- www.syntacticbayleaves.com/js/syntacticbayleaves.js
+++
@@ -1,7 +0,0 @@
-// SyntacticBayLeaves.com
-
-$(document).ready(function() {
-  if (window.location.pathname !== "/") {
-    $('.back-home').html('<a href="/" title="There\'s more bloggliness where that came from."><i class="icon-arrow-left"></i> Go back home</a>')
-  }
-})
